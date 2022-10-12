b4d6f99e9236e2b9d09a1971b9cca0e8ac6bc015
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-12 22:08:49 +0600

    eye button alert add

--- quiz-hero/src/components/Options/Options.js
+++ quiz-hero/src/components/Options/Options.js
@@ -2,21 +2,24 @@ import React from 'react';
 
 const Options = ({ quiz }) => {
     const { correctAnswer, id, question, options } = quiz;
+    const eyeButtonClicked = (ans) => {
+        alert(ans)
+    }
 
 
     return (
-        <div className='m-4 p-4 shadow-lg rounded-lg'>
+        <div className='m-4 p-4 shadow-lg rounded-lg bg-slate-100'>
             <div className=' flex justify-between mb-2'>
                 <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">{question}</h3>
-                <button className='btn btn-info hover:btn-accent'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <button onClick={() => eyeButtonClicked(correctAnswer)} className='btn btn-info hover:btn-accent'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg></button>
 
             </div>
 
-            <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+            <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
+                <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600 ">
                     <div className="flex items-center pl-3">
                         <input id="horizontal-list-radio-license" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
                         <label for="horizontal-list-radio-license" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[0]} </label>

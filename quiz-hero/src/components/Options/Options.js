2e6e1b32b898d155c227904e6d9438d0ecc29264
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-12 22:27:18 +0600

    project done with all bonus content

--- quiz-hero/src/components/Options/Options.js
+++ quiz-hero/src/components/Options/Options.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+
 const Options = ({ quiz }) => {
     const { correctAnswer, id, question, options } = quiz;
     const eyeButtonClicked = (ans) => {
         alert(ans)
+    }
+
+    const isAnswer = (res, cAns) => {
+        if (res === cAns) {
+            alert('Ans is Right')
+        }
+        else {
+            alert('Ans is Wrong')
+        }
     }
 
 
@@ -21,26 +31,26 @@ const Options = ({ quiz }) => {
             <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
                 <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600 ">
                     <div className="flex items-center pl-3">
-                        <input id="horizontal-list-radio-license" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                        <label for="horizontal-list-radio-license" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[0]} </label>
+
+                        <label onClick={() => isAnswer(options[0], correctAnswer)} for="horizontal-list-radio-license" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[0]} </label>
                     </div>
                 </li>
                 <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
                     <div className="flex items-center pl-3">
-                        <input id="horizontal-list-radio-id" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                        <label for="horizontal-list-radio-id" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[1]}</label>
+
+                        <label onClick={() => isAnswer(options[1], correctAnswer)} for="horizontal-list-radio-id" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[1]}</label>
                     </div>
                 </li>
                 <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
                     <div className="flex items-center pl-3">
-                        <input id="horizontal-list-radio-millitary" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                        <label for="horizontal-list-radio-millitary" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[2]}</label>
+
+                        <label onClick={() => isAnswer(options[2], correctAnswer)} for="horizontal-list-radio-millitary" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[2]}</label>
                     </div>
                 </li>
                 <li className="w-full dark:border-gray-600">
                     <div className="flex items-center pl-3">
-                        <input id="horizontal-list-radio-passport" type="radio" value="" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                        <label for="horizontal-list-radio-passport" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[3]}</label>
+
+                        <label onClick={() => isAnswer(options[3], correctAnswer)} for="horizontal-list-radio-passport" className="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{options[3]}</label>
                     </div>
                 </li>
             </ul>

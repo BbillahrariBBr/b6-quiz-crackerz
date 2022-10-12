91d30b3b379a22ffa55920841cef4326d0d6e71e
BbillahrariBBr <mb.billah8@gmail.com>
2022-10-12 16:02:16 +0600

    home page item load nd ui done

--- quiz-hero/src/components/Home/Home.js
+++ quiz-hero/src/components/Home/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 import Hero from '../Hero/Hero';
+import Items from '../Items/Items';
 
 const Home = () => {
+    const items = useLoaderData();
+    const dt = items.data;
+    console.log(dt);
     return (
         <div>
             <Hero></Hero>
+            <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-3'>
+                {
+                    dt.map(item => <Items
+                        key={item.id}
+                        item={item}
+                    ></Items>)
+                }
+            </div>
+
 
         </div>
     );
---
+++ quiz-hero/src/components/Items/Items.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Items.css'
+
+const Items = ({ item }) => {
+    const { name, logo, total } = item
+    return (
+
+        <div className="wrapper text-gray-900  m-4 p-4 rounded-lg ">
+            <div>
+
+                <img src={logo} alt=" random imgee" className="w-full object-cover object-center rounded-lg shadow-md bg-purple-300" />
+
+                <div className="relative px-4 -mt-16  ">
+                    <div className="bg-white p-6 rounded-lg shadow-lg">
+                        <div className="flex items-baseline">
+                            <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
+                                New
+                            </span>
+                            <div className="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider">
+                                Total quiz: {total}
+                            </div>
+                        </div>
+
+                        <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">{name}</h4>
+
+                        <div className="mt-4">
+                            <span className="text-teal-600 text-md font-semibold"><button className='btn'>Start Practice</button></span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Items;
